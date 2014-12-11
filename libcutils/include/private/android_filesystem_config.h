dc7a2feae26a274797699173f6d294b0e8b39abd
Nikhilesh Reddy <reddyn@codeaurora.org>
2014-12-11 13:17:06 -0800

    Allocate a new AIDs for RFS module
    Allocate new AIDs to allow the RFS module
    to run as after dropping privelages from
    ROOT and to enforce strict permissions on
    shared and private RFS files.
    
    Change-Id: Ic4febd565df9f232f6c7571355ec10d8fd2e21bd

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -159,6 +159,9 @@
 #define AID_OEM_RESERVED_2_START 5000
 #define AID_OEM_RESERVED_2_END 5999
 
+#define AID_RFS           3012  /* Remote Filesystem for peripheral processors */
+#define AID_RFS_SHARED    3013  /* Shared files for Remote Filesystem for peripheral processors  */
+
 #define AID_EVERYBODY 9997 /* shared between all apps in the same profile */
 #define AID_MISC 9998      /* access to misc storage */
 #define AID_NOBODY 9999
