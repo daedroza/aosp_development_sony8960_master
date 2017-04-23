a2579566603a4f2954193f941364188a9befe5c1
Adrian DC <radian.dc@gmail.com>
2017-04-23 23:31:15 +0200

    libril: Sony 8960 RIL stack changes to RIL_SIM_IO v5 and v6
     * Match the custom stack modifications used upon
        original compilation from the proprietary RIL stack
    
    Change-Id: I3216d507280d87d62d2c884182c22d8d30010ea6
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -599,6 +599,7 @@ typedef struct {
 } RIL_Dial;
 
 typedef struct {
+    int cla;        /* Sony 8960 RIL stack compatibility offset */
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
@@ -613,6 +614,7 @@ typedef struct {
 } RIL_SIM_IO_v5;
 
 typedef struct {
+    int cla;        /* Sony 8960 RIL stack compatibility offset */
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
