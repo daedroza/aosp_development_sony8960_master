c2134ddca61e8d7f05671738bb357516fbd5cda8
Adrian DC <radian.dc@gmail.com>
2017-05-01 19:59:46 +0200

    TOTEST: display test
    Change-Id: I55aea7480bf987d6ebc8fac2e18628830f8ff8d2

--- board/qcom-display.mk
+++ board/qcom-display.mk
@@ -1,8 +1,6 @@
 # Display
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 
-# Adreno driver
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
 # Ion
+TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
