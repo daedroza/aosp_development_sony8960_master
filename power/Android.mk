6eeeaca86502295c05cfb556d7308381c17a0607
Adrian DC <radian.dc@gmail.com>
2017-05-07 19:08:58 +0200

    qcom: power: Move power.qcom.so to /system/lib/hw
     * Also use the new LOCAL_MODULE_RELATIVE_PATH
        configuration instead of a full path
    
    Change-Id: I80bc5bfbbdf32b55dd2ee2dbd8961b9c1f05ca34
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -7,7 +7,7 @@ ifeq ($(TARGET_DEVICE),huashan)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 LOCAL_MODULE := power.qcom
